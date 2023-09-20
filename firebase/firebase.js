caa16c9a3d5f7f224d9de2845f58ffb75eee1c85
Hubert Warchoł <n3tr00x99@gmail.com>
2023-09-21 01:49:53 +0200

    added database url to firebase config

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { getDatabase } from 'firebase/database';
 
 // Get values from enviormental variables
 const {
@@ -9,6 +10,7 @@ const {
 	VITE_FIREBASE_STORAGE_BUCKET,
 	VITE_FIRBASE_MESSAGING_SENDER_ID,
 	VITE_FIREBASE_APP_ID,
+	VITE_FIREBASE_DATABASE_URL,
 } = import.meta.env;
 
 // Firebase configuration
@@ -19,7 +21,9 @@ const firebaseConfig = {
 	storageBucket: VITE_FIREBASE_STORAGE_BUCKET,
 	messagingSenderId: VITE_FIRBASE_MESSAGING_SENDER_ID,
 	appId: VITE_FIREBASE_APP_ID,
+	databaseURL: VITE_FIREBASE_DATABASE_URL,
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const database = getDatabase(app);
