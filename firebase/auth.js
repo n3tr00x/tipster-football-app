ec4586d4628fdb186d26624bebb872945276bbf1
Hubert Warchoł <n3tr00x99@gmail.com>
2023-09-21 02:18:12 +0200

    added a function for updating user data
    the ability to change the username, avatar, password, email, and log out.

--- firebase/auth.js
+++ firebase/auth.js
@@ -1,10 +1,14 @@
 import { auth } from './firebase';
 import {
+	EmailAuthProvider,
 	createUserWithEmailAndPassword,
+	reauthenticateWithCredential,
 	sendEmailVerification,
 	sendPasswordResetEmail,
 	signInWithEmailAndPassword,
 	signOut,
+	updateEmail,
+	updatePassword,
 	updateProfile,
 } from 'firebase/auth';
 
@@ -48,6 +52,8 @@ export const registerUser = async ({
 
 		await updateProfile(user, {
 			displayName: username,
+			photoURL:
+				'https://cdn.pixabay.com/photo/2014/04/02/10/25/man-303792_1280.png',
 		});
 
 		await sendEmailVerification(user, {
@@ -125,4 +131,84 @@ export const forgotPassword = async ({ email }) => {
 				throw new AuthError('Nieznany błąd.');
 		}
 	}
+};
+
+export const updateProfileDetails = updatedData => {
+	return updateProfile(auth.currentUser, {
+		...updatedData,
+	});
+};
+
+export const updateUserEmail = async ({ email, password }) => {
+	try {
+		const credential = EmailAuthProvider.credential(
+			auth.currentUser.email,
+			password
+		);
+
+		await reauthenticateWithCredential(auth.currentUser, credential);
+
+		await updateEmail(auth.currentUser, email);
+		await sendEmailVerification(auth.currentUser, {
+			url: `${window.location.origin}/sign-in`,
+			handleCodeInApp: true,
+		});
+
+		signOut(auth);
+	} catch (error) {
+		console.log(error);
+		switch (error.code) {
+			case 'auth/invalid-credential':
+				throw new AuthError('Nastąpiła zła weryfikacja konta!');
+			case 'auth/wrong-password':
+				throw new AuthError('Podano złe aktualne hasło!');
+			case 'auth/passwords-not-identical':
+				throw new AuthError('Hasła nie są identyczne!');
+			case 'auth/weak-password':
+				throw new AuthError(
+					'Hasło jest zbyt słabe. Musi składać się z minimum 6 znaków.'
+				);
+			default:
+				throw new AuthError('Nieznany błąd.');
+		}
+	}
+};
+
+export const updateUserPassword = async ({
+	email,
+	currentPassword,
+	password,
+	confirmPassword,
+}) => {
+	try {
+		if (password !== confirmPassword) {
+			throw {
+				code: 'auth/passwords-not-identical',
+			};
+		}
+
+		const credential = EmailAuthProvider.credential(email, currentPassword);
+		await reauthenticateWithCredential(auth.currentUser, credential);
+
+		updatePassword(auth.currentUser, password);
+	} catch (error) {
+		switch (error.code) {
+			case 'auth/invalid-credential':
+				throw new AuthError('Nastąpiła zła weryfikacja konta!');
+			case 'auth/wrong-password':
+				throw new AuthError('Podano złe aktulne konto');
+			case 'auth/passwords-not-identical':
+				throw new AuthError('Hasła nie są identyczne!');
+			case 'auth/weak-password':
+				throw new AuthError(
+					'Hasło jest zbyt słabe. Musi składać się z minimum 6 znaków.'
+				);
+			default:
+				throw new AuthError('Nieznany błąd.');
+		}
+	}
+};
+
+export const logout = async () => {
+	await signOut(auth);
 };
